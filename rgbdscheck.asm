7887c07bd31073211edeaaf5a0b788b6a2546a52
just_late <222404211+just-late@users.noreply.github.com>
2025-12-02 12:12:42 -0500

    Build with RGBDS v1.0.0

--- rgbdscheck.asm
+++ rgbdscheck.asm
@@ -1,3 +1,3 @@
-if __RGBDS_MAJOR__ == 0 && __RGBDS_MINOR__ < 8
-	fail "polishedcrystal requires rgbds v0.8.0 or newer."
+if __RGBDS_MAJOR__ == 1 && __RGBDS_MINOR__ < 0
+	fail "pokelavender requires rgbds v1.0.0 or newer."
 endc
