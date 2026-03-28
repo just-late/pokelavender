51916ba3d2740ed75fe5fe068ae8a614d77064f7
just_late <222404211+just-late@users.noreply.github.com>
2026-03-28 08:26:38 -0400

    took a load of screen shots lol

---
+++ audio/music/countbubbatime.asm
@@ -0,0 +1,303 @@
+Music_UntitledSong:
+	channel_count 4
+	channel 1, Music_UntitledSong_Ch1
+	channel 2, Music_UntitledSong_Ch2
+	channel 3, Music_UntitledSong_Ch3
+	channel 4, Music_UntitledSong_Ch4
+
+Music_UntitledSong_Ch1:
+	tempo 256
+	volume 7, 7
+	note_type 12, 15, 8
+.mainLoop:
+	octave 3
+	tempo 240
+	rest 1
+	tempo 150
+	duty_cycle 1
+	note F_, 1
+	note F_, 2
+	rest 1
+	note F_, 1
+	note F_, 2
+	rest 1
+	note F_, 1
+	note F_, 2
+	rest 1
+	note F_, 1
+	note F_, 2
+	rest 1
+	note F_, 1
+	note F_, 2
+	rest 1
+	note F_, 1
+	octave 4
+	note_type 12, 15, 8
+	note E_, 2
+	note F_, 8
+	note D_, 2
+	note C_, 2
+	rest 1
+	note C_, 1
+	note E_, 2
+	rest 1
+	octave 5
+	note D_, 1
+	note C_, 2
+	rest 1
+	note C_, 1
+	note C_, 2
+	rest 1
+	note C_, 1
+	note C_, 2
+	rest 1
+	note C_, 1
+	octave 4
+	note A_, 1
+	note G_, 1
+	octave 3
+	volume_envelope 7, 8
+	note A_, 4
+	note F_, 4
+	note C_, 4
+	note F_, 4
+	note A_, 4
+	note D_, 4
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	octave 8
+	sound_loop 0, .mainLoop
+
+Music_UntitledSong_Ch2:
+	note_type 12, 15, 8
+.mainLoop:
+	rest 1
+	octave 3
+	note C_, 1
+	octave 2
+	duty_cycle 1
+	note C_, 2
+	octave 3
+	rest 1
+	note C_, 1
+	octave 2
+	note F_, 2
+	rest 1
+	octave 3
+	note C_, 1
+	octave 2
+	note C_, 2
+	octave 3
+	rest 1
+	note C_, 1
+	octave 2
+	note F_, 2
+	rest 1
+	octave 3
+	note C_, 1
+	octave 2
+	note C_, 2
+	octave 3
+	rest 1
+	note C_, 1
+	octave 4
+	note C_, 10
+	octave 3
+	note A#, 2
+	note A_, 2
+	octave 2
+	note F_, 2
+	rest 2
+	note C_, 2
+	octave 4
+	note F_, 2
+	octave 2
+	note F_, 2
+	octave 4
+	note F_, 2
+	octave 2
+	note C_, 2
+	octave 4
+	note F_, 2
+	rest 1
+	note F_, 1
+	note F_, 2
+	note D_, 4
+	octave 3
+	note A#, 1
+	note A#, 3
+	note G#, 2
+	note F_, 2
+	note D_, 4
+	note D#, 1
+	note E_, 1
+	note F_, 2
+	note A#, 2
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 2
+	octave 8
+	sound_loop 0, .mainLoop
+
+Music_UntitledSong_Ch3:
+	note_type 12, 1, 0
+.mainLoop:
+	rest 1
+	rest 1
+	octave 4
+	note G_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	note G_, 2
+	note C_, 2
+	octave 2
+	note F_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	note F_, 2
+	note C_, 2
+	rest 2
+	volume_envelope 1, 2
+	note A_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	volume_envelope 3, 0
+	note A_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	octave 4
+	note A_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note D_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note A_, 2
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 6
+	octave 8
+	sound_loop 0, .mainLoop
+
+Music_UntitledSong_Ch4:
+	toggle_noise 0
+	drum_speed 12
+.mainLoop:
+	rest 1
+	octave 1
+	toggle_noise
+	toggle_noise 3
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 2
+	drum_note 8, 1
+	drum_note 8, 1
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	octave 8
+	rest 16
+	rest 13
+	sound_loop 0, .mainLoop
