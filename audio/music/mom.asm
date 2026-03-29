ca2b72b32523565ee97b4cd0d8ee1ff6b9389b3c
just_late <222404211+just-late@users.noreply.github.com>
2026-03-29 12:52:46 -0400

    rewrote mom music

--- audio/music/mom.asm
+++ audio/music/mom.asm
@@ -6,252 +6,282 @@ Music_Mom:
 	channel 4, Music_Mom_Ch4
 
 Music_Mom_Ch1:
-	tempo 240
+	tempo 144
 	volume 7, 7
-	note_type 12, 15, 8
-Music_Mom_Ch1_mainLoop:
-	octave 2
-	vibrato 0, 2, 1
-	volume_envelope 15, 8
-	note G_, 1
-	rest 1
-	note_type 12, 15, 8
-	duty_cycle 0
-	rest 1
-	note G_, 1
-	rest 2
-	note G_, 1
+	stereo_panning FALSE, TRUE
+	duty_cycle 2
+	note_type 6, 11, 3
 	octave 4
-	rest 1
 	note E_, 1
-	rest 1
+	note G#, 1
+	note A_, 1
+	octave 5
+	note E_, 5
+	octave 4
+Music_Mom_branch_f6c67:
+	note_type 12, 11, 1
+	rest 5
+	note_type 6, 11, 1
+	octave 3
+	rest 2
+	volume_envelope 9, 1
 	note E_, 2
-	note D_, 1
-	note C_, 1
-	rest 1
-	octave 2
-	note B_, 1
+	note_type 12, 11, 1
 	rest 2
-	note B_, 1
-	note_type 1, 15, 8
-	rest 12
-	note_type 12, 15, 8
-	rest 1
-	note B_, 1
-	rest 1
-	octave 4
-	note G_, 1
-	rest 1
-	note G_, 2
-	note F_, 1
-	note E_, 1
-	octave 2
-	rest 1
+	note_type 6, 11, 1
+	rest 2
+	note G#, 1
 	note A_, 1
 	rest 2
-	note A_, 1
+	note F#, 2
 	rest 2
-	note A_, 1
+	note E_, 2
 	rest 1
+	note_type 1, 11, 1
+	rest 6
+	note_type 12, 11, 1
+	rest 2
+	note_type 6, 11, 1
 	octave 4
 	note F_, 1
-	rest 1
-	note F_, 2
 	note E_, 1
-	note D_, 1
+	note_type 12, 11, 1
 	rest 1
-	octave 2
-	note B_, 1
+	note_type 6, 11, 1
+	note C#, 2
 	rest 2
-	note B_, 1
+	octave 3
+	note B_, 2
 	rest 2
-	note B_, 1
 	octave 4
+	note C#, 2
+	note_type 12, 11, 1
 	rest 1
-	note G_, 1
-	rest 1
-	note G_, 2
-	note F_, 1
-	note E_, 1
-	note C_, 4
+	note_type 6, 11, 1
+	octave 3
+	note A_, 4
+	note E_, 2
+	note D_, 2
+	note_type 1, 11, 1
 	rest 16
+	rest 8
+	note_type 6, 11, 1
+	note E_, 2
+	note_type 12, 11, 1
+	rest 5
+	note_type 6, 11, 1
+	note E_, 2
+	note_type 12, 11, 1
 	rest 1
-	sound_jump Music_Mom_Ch1_mainLoop
+	note_type 6, 11, 1
+	note F#, 2
+	note_type 12, 11, 1
+	rest 1
+	note_type 6, 11, 1
+	rest 4
+	octave 4
+	note C_, 4
+	octave 3
+	note A_, 2
+	note_type 12, 11, 1
+	rest 1
+	note_type 6, 11, 1
+	note F#, 2
+	rest 2
+	note A#, 1
+	note A_, 1
+	note G#, 1
+	note G_, 1
+	note F#, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note A_, 2
+	note_type 12, 11, 1
+	rest 3
+	note_type 6, 11, 1
+	note B_, 1
+	note A#, 1
+	note A_, 1
+	note_type 1, 11, 1
+	rest 6
+	note_type 6, 11, 1
+	octave 4
+	note E_, 2
+	octave 3
+	note_type 12, 11, 1
+	rest 4
+	note_type 1, 11, 1
+	rest 12
+	note_type 12, 11, 1
+	sound_jump Music_Mom_branch_f6c67
 
 Music_Mom_Ch2:
-	note_type 12, 15, 8
-Music_Mom_Ch2_mainLoop:
+	volume 7, 7
+	stereo_panning FALSE, TRUE
+	duty_cycle 2
+	note_type 6, 11, 3
+	octave 3
+	note B_, 1
+	octave 4
+	note E_, 1
+	note G#, 1
+	note A_, 5
+Music_Mom_branch_f6c0e:
+	note_type 12, 11, 1
+	rest 4
+	octave 3
+	note C#, 6
+	note D_, 4
+	note C_, 2
+	rest 2
+	note C#, 6
 	octave 2
-	volume_envelope 10, 8
-	vibrato 0, 0, 0
-	note E_, 1
+	note E_, 4
+	note A#, 2
+	note B_, 2
+	rest 4
+	octave 3
+	note C#, 6
+	note D_, 4
+	note C_, 2
 	rest 2
-	note E_, 1
-	rest 2
-	note E_, 1
-	rest 8
-	note G_, 1
-	rest 2
-	note G_, 1
-	rest 2
-	note G_, 1
-	note_type 12, 15, 8
-	rest 8
-	note E_, 1
-	rest 2
-	note E_, 1
-	rest 2
-	note E_, 1
-	rest 8
-	note G_, 1
-	rest 2
-	note G_, 1
-	rest 2
-	note G_, 1
-	rest 16
-	rest 12
-	sound_jump Music_Mom_Ch2_mainLoop
+	note C#, 6
+	octave 2
+	note E_, 4
+	note A#, 2
+	note B_, 2
+	rest 4
+	sound_jump Music_Mom_branch_f6c0e
 
 Music_Mom_Ch3:
-	note_type 12, 1, 0
-Music_Mom_Ch3_mainLoop:
+	stereo_panning TRUE, FALSE
+	note_type 6, 2, 5
+	octave 3
+	note E_, 1
+	note D_, 1
+	octave 2
+	note B_, 1
+	note A_, 5
+	note_type 6, 2, 3
+Music_Mom_branch_f6c2e:
+	octave 2
+	note A_, 2
+	rest 6
+	octave 4
+	note E_, 2
+	rest 2
+	octave 2
+	note A_, 2
 	rest 2
 	octave 3
-	note C_, 1
+	note D_, 2
 	rest 2
-	note C_, 1
+	octave 4
+	note F#, 8
+	note D#, 2
 	rest 2
-	note C_, 1
+	octave 2
+	note A_, 2
 	rest 2
-	note C_, 1
+	octave 4
+	note E_, 2
+	rest 6
+	octave 2
+	note A_, 2
 	rest 2
-	note C_, 1
+	octave 4
+	note D_, 2
+	rest 6
+	note C#, 4
+	note D_, 2
 	rest 2
-	note E_, 1
+	note_type 6, 2, 3
+	octave 2
+	note A_, 2
+	rest 6
+	octave 4
+	note E_, 2
+	octave 2
 	rest 2
-	note E_, 1
+	note A_, 2
 	rest 2
-	note E_, 1
+	octave 3
+	note D_, 2
 	rest 2
-	note E_, 1
-	note_type 12, 1, 3
+	octave 4
+	note F#, 8
+	note D#, 2
 	rest 2
-	volume_envelope 1, 0
-	note E_, 1
+	octave 2
+	note A_, 2
+	octave 4
 	rest 2
-	vibrato 0, 0, 0
-	note D_, 1
+	note E_, 2
+	rest 6
+	octave 2
+	note A_, 2
+	octave 4
 	rest 2
-	note D_, 1
-	rest 2
-	note D_, 1
-	rest 2
-	note D_, 1
-	rest 2
-	note D_, 1
-	rest 2
-	note E_, 1
-	rest 2
-	note E_, 1
-	rest 2
-	note E_, 1
-	rest 2
-	note E_, 1
-	rest 2
-	note E_, 1
-	rest 2
-	note E_, 1
-	rest 16
-	rest 1
-	sound_jump Music_Mom_Ch3_mainLoop
+	note D_, 2
+	rest 6
+	note C#, 4
+	note D_, 2
+	rest 10
+	sound_jump Music_Mom_branch_f6c2e
 
 Music_Mom_Ch4:
-	toggle_noise 0
+	toggle_noise 3
 	drum_speed 12
-Music_Mom_Ch4_mainLoop:
-	octave 3
-	toggle_noise
-	toggle_noise 0
+	rest 4
+Music_Mom_branch_f6c56:
 	drum_note 4, 1
 	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
+	drum_note 7, 2
+	drum_note 3, 1
+	rest 1
+	drum_note 8, 1
+	rest 1
+	drum_note 8, 1
+	rest 1
+	drum_note 7, 4
+	drum_note 3, 1
 	rest 1
 	drum_note 4, 1
 	rest 1
-	drum_note 4, 1
+	drum_note 7, 4
+	drum_note 3, 1
+	rest 1
+	drum_note 8, 1
+	rest 1
+	drum_note 8, 1
+	rest 1
+	drum_note 3, 2
 	drum_note 4, 1
 	rest 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
 	drum_speed 12
-	rest 1
-	drum_note 4, 1
 	drum_note 4, 1
 	rest 1
-	drum_note 4, 1
+	drum_note 7, 2
+	drum_note 3, 1
 	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
+	drum_note 8, 1
 	rest 1
-	drum_note 4, 1
+	drum_note 8, 1
 	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
+	drum_note 7, 4
+	drum_note 3, 1
 	rest 1
 	drum_note 4, 1
 	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
+	drum_note 7, 4
+	drum_note 3, 1
 	rest 1
-	drum_note 4, 1
+	drum_note 8, 1
 	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
+	drum_note 8, 1
 	rest 1
+	drum_note 3, 2
 	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	rest 1
-	drum_note 4, 1
-	drum_note 4, 1
-	rest 1
-	octave 8
-	sound_jump Music_Mom_Ch4_mainLoop
+	rest 5
+	sound_jump Music_Mom_branch_f6c56
