93e848af6da175d3ed80f36ccbdae856076f1587
just_late <222404211+just-late@users.noreply.github.com>
2026-03-30 08:17:45 -0400

    started writing new rival fight and mart music

---
+++ audio/music/hgss/mart1.asm
@@ -0,0 +1,324 @@
+Music_mart:
+	channel_count 4
+	channel 1, Music_mart_Ch1
+	channel 2, Music_mart_Ch2
+	channel 3, Music_mart_Ch3
+	channel 4, Music_mart_Ch4
+
+Music_mart_Ch1:
+	volume 7, 7
+	octave 5
+	note_type 6, 15, 8
+	tempo 256
+	duty_cycle 2
+	note C_, 1
+	note E_, 1
+	note F_, 1
+	note G_, 1
+	octave 6
+	note C_, 8
+	octave 5
+	note C_, 4
+	octave 6
+	note_type 12, 15, 8
+.mainLoop:
+	note_type 6, 15, 8
+	rest 4
+	octave 4
+	duty_cycle 3
+	note A_, 8
+	note G_, 8
+	note E_, 4
+	note F_, 4
+	note G_, 4
+	note A_, 4
+	note A#, 2
+	octave 1
+	note A_, 1
+	octave 4
+	rest 16
+	rest 1
+	note E_, 4
+	note G_, 6
+	rest 2
+	note G_, 6
+	rest 2
+	note G_, 4
+	note F_, 4
+	note E_, 4
+	note G_, 4
+	note F_, 16
+	octave 6
+	note_type 12, 15, 8
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 2
+	sound_loop 0, .mainLoop
+
+Music_mart_Ch2:
+	octave 4
+	note_type 6, 15, 1
+	duty_cycle 2
+	note C_, 1
+	note D_, 1
+	note D#, 1
+	note_type 6, 15, 8
+	note E_, 1
+	note F_, 8
+	duty_cycle 3
+	octave 3
+	note F_, 4
+	octave 5
+	note_type 12, 15, 8
+.mainLoop:
+	rest 1
+	octave 4
+	note_type 6, 15, 1
+	vibrato 3, 0, 0
+	duty_cycle 2
+	sound_call .sub1
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	octave 3
+	note G_, 2
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	note G_, 2
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	octave 4
+	note C_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	octave 3
+	note G_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	octave 4
+	note C_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	octave 3
+	note G_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note A#, 2
+	rest 2
+	note G_, 2
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	note A_, 2
+	note_type 12, 15, 8
+	rest 1
+	octave 4
+	note_type 6, 15, 8
+	note C_, 2
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	note C_, 2
+	note_type 12, 15, 8
+	rest 1
+	octave 3
+	note_type 6, 15, 8
+	note A_, 2
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	note A_, 2
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	octave 4
+	note C_, 2
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	note C_, 2
+	note_type 12, 15, 8
+	rest 1
+	octave 3
+	note_type 6, 15, 8
+	note A_, 2
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	note A_, 2
+	note_type 12, 15, 8
+	octave 5
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 8
+	sound_loop 0, .mainLoop
+
+.sub1:
+	note C_, 2
+	rest 2
+	note C_, 2
+	note_type 12, 15, 8
+	rest 1
+	octave 3
+	note_type 6, 15, 8
+	note G_, 2
+	rest 2
+	note G_, 2
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	octave 4
+	note C_, 2
+	rest 2
+	note C_, 2
+	octave 3
+	note_type 12, 15, 8
+	rest 1
+	note_type 6, 15, 8
+	note G_, 2
+	rest 1
+	note_type 6, 10, 8
+	rest 1
+	volume_envelope 15, 8
+	note G_, 2
+	note_type 12, 15, 8
+	rest 1
+	octave 4
+	note_type 6, 15, 8
+	note C_, 2
+	note_type 6, 10, 8
+	rest 2
+	volume_envelope 15, 8
+	note C_, 2
+	sound_ret
+
+Music_mart_Ch3:
+	note_type 12, 1, 0
+	rest 2
+	octave 5
+	note A_, 4
+	octave 4
+	note A_, 2
+	octave 8
+.mainLoop:
+	octave 3
+	volume_envelope 1, 6
+	sound_call .sub1
+	sound_call .sub1
+	note_type 12, 1, 6
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	octave 8
+	rest 16
+	rest 16
+	note_type 12, 1, 0
+	rest 8
+	sound_loop 0, .mainLoop
+
+.sub1:
+	note F_, 2
+	rest 1
+	note F_, 1
+	note C_, 2
+	rest 1
+	note C_, 1
+	note F_, 2
+	rest 1
+	note F_, 1
+	note C_, 2
+	rest 1
+	note C_, 1
+	note G_, 2
+	rest 1
+	note G_, 1
+	note C_, 2
+	rest 1
+	note C_, 1
+	note G_, 2
+	rest 1
+	note G_, 1
+	note C_, 2
+	rest 1
+	note C_, 1
+	sound_ret
+
+Music_mart_Ch4:
+	toggle_noise 0
+	drum_speed 12
+	rest 2
+	toggle_noise
+	toggle_noise 5
+	octave 7
+	drum_note 1, 1
+	rest 2
+	drum_note 1, 1
+	drum_note 1, 2
+	octave 8
+.mainLoop:
+	octave 6
+	sound_call .sub1
+	sound_call .sub1
+	drum_speed 12
+	sound_call .sub1
+	drum_speed 12
+	sound_call .sub1
+	drum_speed 12
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	octave 8
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 8
+	sound_loop 0, .mainLoop
+
+.sub1:
+	drum_note 6, 3
+	drum_note 10, 1
+	drum_note 6, 3
+	drum_note 10, 1
+	drum_note 6, 3
+	drum_note 10, 1
+	drum_note 6, 3
+	drum_note 10, 1
+	sound_ret
---
+++ audio/music/rivalfight1.asm
@@ -0,0 +1,332 @@
+Music_UntitledSong:
+	channel_count 4
+	channel 1, Music_UntitledSong_Ch1
+	channel 2, Music_UntitledSong_Ch2
+	channel 3, Music_UntitledSong_Ch3
+	channel 4, Music_UntitledSong_Ch4
+
+Music_UntitledSong_Ch1:
+	volume 7, 7
+	note_type 12, 15, 8
+	octave 4
+	tempo 120
+	note F_, 1
+	note F#, 1
+	note G_, 1
+	note G#, 1
+	note F#, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	note A#, 1
+	note G#, 1
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	octave 5
+	note C_, 1
+	octave 4
+	note A#, 1
+	note B_, 1
+	octave 5
+	note C_, 1
+	note C#, 1
+	octave 4
+	note B_, 1
+	octave 5
+	note C_, 1
+	note C#, 1
+	note D_, 1
+	note C_, 1
+	note C#, 1
+	note D_, 1
+	note E_, 9
+	octave 3
+	vibrato 2, 1, 0
+	duty_cycle 2
+	volume_envelope 10, 1
+	note E_, 4
+	rest 2
+	note E_, 4
+	note G#, 3
+	rest 1
+	note E_, 4
+	rest 2
+	note C#, 4
+	octave 8
+.mainLoop:
+	octave 3
+	volume_envelope 15, 1
+	duty_cycle 2
+	vibrato 2, 2, 2
+	note F_, 4
+	rest 2
+	note E_, 4
+	rest 2
+	note C#, 4
+	note D_, 3
+	rest 1
+	note E_, 4
+	rest 2
+	note E_, 4
+	note B_, 4
+	octave 5
+	volume_envelope 10, 8
+	note E_, 12
+	note F_, 4
+	note E_, 4
+	note D_, 4
+	note E_, 4
+	note G#, 8
+	volume_envelope 15, 8
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	octave 8
+	rest 16
+	rest 16
+	rest 16
+	rest 10
+	sound_loop 0, .mainLoop
+
+Music_UntitledSong_Ch2:
+	note_type 12, 10, 8
+	octave 3
+	duty_cycle 3
+	note B_, 1
+	note A#, 1
+	note A_, 1
+	note G#, 1
+	note A#, 1
+	note A_, 1
+	note G#, 1
+	note G_, 1
+	note A_, 1
+	note G#, 1
+	note G_, 1
+	note F#, 1
+	note G#, 1
+	note G_, 1
+	note F#, 1
+	note F_, 1
+	note G_, 1
+	note F#, 1
+	note F_, 1
+	note E_, 1
+	note F#, 1
+	note F_, 1
+	note E_, 1
+	note D#, 1
+	note F_, 1
+	note F#, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	note B_, 1
+	octave 4
+	note D_, 1
+	octave 5
+	note C_, 9
+	octave 4
+	volume_envelope 10, 1
+	duty_cycle 2
+	note E_, 4
+	rest 2
+	note E_, 4
+	note F_, 3
+	rest 1
+	note E_, 4
+	rest 2
+	note E_, 4
+	octave 8
+.mainLoop:
+	octave 4
+	note D_, 4
+	rest 2
+	note E_, 4
+	rest 2
+	note E_, 4
+	note F_, 3
+	rest 1
+	note E_, 4
+	rest 2
+	note E_, 4
+	note G#, 4
+	rest 2
+	note E_, 4
+	rest 2
+	note E_, 4
+	note F_, 3
+	rest 1
+	note E_, 4
+	rest 2
+	note E_, 4
+	note D_, 4
+	rest 2
+	note F_, 4
+	rest 16
+	rest 16
+	rest 16
+	octave 8
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 10
+	sound_loop 0, .mainLoop
+
+Music_UntitledSong_Ch3:
+	note_type 12, 1, 0
+	octave 3
+	note B_, 2
+	note A_, 2
+	octave 4
+	note C_, 2
+	octave 3
+	note F_, 2
+	octave 4
+	note D_, 2
+	note F_, 2
+	note C_, 2
+	octave 3
+	note A_, 2
+	octave 4
+	note C_, 2
+	note D_, 2
+	note F_, 2
+	note E_, 2
+	note G#, 2
+	note F#, 2
+	note E_, 2
+	note F#, 1
+	note A_, 9
+	vibrato 0, 1, 0
+	volume_envelope 1, 12
+	note E_, 2
+	note C#, 2
+	note E_, 2
+	note C#, 2
+	note E_, 2
+	note F_, 2
+	note D_, 2
+	note E_, 2
+	note C#, 2
+	note E_, 2
+	note C#, 2
+	note E_, 2
+	octave 8
+.mainLoop:
+	octave 4
+	note F_, 2
+	note D_, 2
+	note F_, 2
+	note E_, 2
+	note C_, 2
+	note E_, 2
+	note C#, 2
+	note E_, 2
+	note F_, 2
+	note D_, 2
+	note E_, 2
+	note C#, 2
+	note E_, 2
+	note C_, 2
+	note E_, 2
+	note F_, 2
+	note D_, 2
+	note E_, 2
+	note C_, 2
+	volume_envelope 1, 12
+	note C#, 2
+	note E_, 2
+	note C#, 2
+	note E_, 2
+	note D_, 2
+	note G#, 2
+	note E_, 2
+	note C#, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	note F_, 2
+	note D_, 2
+	note F_, 2
+	note G#, 2
+	note C#, 2
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 10
+	octave 8
+	sound_loop 0, .mainLoop
+
+Music_UntitledSong_Ch4:
+	toggle_noise 0
+	drum_speed 12
+	rest 16
+	rest 16
+	rest 8
+	octave 6
+	toggle_noise
+	toggle_noise 1
+	drum_note 1, 4
+	octave 5
+	drum_note 12, 4
+	octave 6
+	drum_note 1, 4
+	drum_note 3, 4
+	drum_note 1, 4
+	octave 5
+	drum_note 12, 4
+	octave 8
+.mainLoop:
+	octave 6
+	toggle_noise
+	toggle_noise 1
+	drum_note 1, 4
+	drum_note 3, 4
+	drum_note 1, 4
+	octave 5
+	drum_note 12, 4
+	octave 6
+	drum_note 1, 4
+	drum_note 3, 4
+	drum_note 1, 4
+	octave 5
+	drum_note 12, 4
+	octave 6
+	drum_note 1, 4
+	drum_note 3, 4
+	drum_note 1, 4
+	octave 5
+	drum_note 12, 4
+	octave 6
+	drum_note 1, 4
+	drum_note 3, 4
+	drum_note 1, 4
+	octave 5
+	drum_note 12, 4
+	octave 6
+	drum_note 1, 4
+	drum_note 3, 4
+	rest 16
+	octave 8
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 8
+	sound_loop 0, .mainLoop
