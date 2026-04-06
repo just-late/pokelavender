20143419e63cd4bce89efc1d84596782c1bbdcd1
just_late <222404211+just-late@users.noreply.github.com>
2026-04-05 20:48:07 -0400

    Added more NPCs to Marigold Port + tweaked the map

--- maps/MarigoldPort.asm
+++ maps/MarigoldPort.asm
@@ -11,12 +11,16 @@ MarigoldPort_MapScriptHeader:
     def_bg_events
 	bg_event 36, 12, BGEVENT_JUMPTEXT, MarigoldPortSignText
 
-    db 4 ; object_events
+    db 9 ; object_events
+	person_event SPRITE_LASS, 13, 23, SPRITEMOVEDATA_WALK_UP_DOWN, 2, 2, -1, -1, PAL_NPC_BLUE, PERSONTYPE_COMMAND, jumptextfaceplayer, MarigoldPortNPC1Text, -1
+	person_event SPRITE_BUG_MANIAC, 11, 28, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 2, -1, -1, PAL_NPC_BROWN, PERSONTYPE_COMMAND, jumptextfaceplayer, MarigoldPortNPC2Text, -1
+	person_event SPRITE_CHILD, 16, 29, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 1, -1, -1, PAL_NPC_BLUE, PERSONTYPE_COMMAND, jumptextfaceplayer, MarigoldPortNPC3Text, -1
+	person_event SPRITE_BEACH_GUY, 10, 11, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, PERSONTYPE_COMMAND, jumptextfaceplayer, MarigoldPortNPC4Text, -1
 	person_event SPRITE_SAILOR, 20, 34, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, PERSONTYPE_COMMAND, jumptextfaceplayer, MarigoldPortSailor1Text, -1
 	person_event SPRITE_SAILOR, 19, 31, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, PERSONTYPE_COMMAND, jumptextfaceplayer, MarigoldPortSailor2Text, -1
 	person_event SPRITE_CUTE_GIRL, 23, 15, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_PURPLE, PERSONTYPE_COMMAND, jumptextfaceplayer, MarigoldPortGirlText, -1
 	pokemon_event 14, 23, FURRET, SPRITEMOVEDATA_POKEMON, -1, -1, PAL_NPC_BROWN, MarigoldPortFurretText, -1
-    pokemon_event 33, 19, MACHOKE, SPRITEMOVEDATA_POKEMON, -1, -1, PAL_NPC_GRAY, MarigoldPortMachokeText, -1
+	pokemon_event 33, 19, MACHOKE, SPRITEMOVEDATA_POKEMON, -1, -1, PAL_NPC_GRAY, MarigoldPortMachokeText, -1
 
 MarigoldPortSailor1Text:
     text "Ain't the salty"
@@ -57,4 +61,39 @@ MarigoldPortMachokeText:
     text "MA!"
     
     para "CHO!"
+    done
+
+MarigoldPortNPC1Text:
+    text "I thought the"
+    line "beach would be"
+    cont "full of people."
+
+    para "This is quite"
+    line "pleasant!"
+    done
+
+MarigoldPortNPC2Text:
+    text "I like to fish on"
+    line "the pier."
+
+    para "But some days, all"
+    line "I catch is"
+    cont "MAGIKARP."
+    done
+
+MarigoldPortNPC3Text:
+    text "Are you a"
+    line "#MON TRAINER?"
+
+    para "…"
+
+    para "Wow! Cool!"
+    done
+
+MarigoldPortNPC4Text:
+    text "I love the beach!"
+    line "It's so calm and…"
+
+    para "OW! A KRABBY"
+    line "pinched me!"
     done
